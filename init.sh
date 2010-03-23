0093a95896dcd7df285d60f177213a5d9e178e35
vladimir <vladimir@sausalito>
2010-03-23 23:55:35 +0100

    update to Mercurial urls

--- init.sh
+++ init.sh
@@ -3,18 +3,21 @@
 for file in $( find -maxdepth 1 -type d )
 do
 
-  if [ "$file" != "." ] 
-  then
-    echo "processing $file"
-    if [ ! -e "$file/conf" ]
-    then
-      mkdir "$file/conf"
-    fi
+  -- not very elegant, but it works...
 
-    svn propset svn:externals -F svn-externals.txt "$file/conf"
-    cp config/conf/ant/project-targets.xml "$file/conf/."
-    cp config/conf/ivy/ivysettings.xml "$file/conf/."
-    cp config/conf/docbook/ivy.xml "$file/conf/docbook-ivy.xml"
+  if [ "$file" != "." ] ; then
+    if [ "$file" != "./.hg" ] ; then
+      if [ "$file" != "./config" ] ; then
+        echo "processing $file"
+        if [ ! -e "$file/conf" ] ; then
+          mkdir "$file/conf"
+        fi
+
+        cp config/conf/ant/project-targets.xml "$file/conf/."
+        cp config/conf/ivy/ivysettings.xml "$file/conf/."
+        cp config/conf/docbook/ivy.xml "$file/conf/docbook-ivy.xml"
+      fi
+    fi
   fi
 
 done
