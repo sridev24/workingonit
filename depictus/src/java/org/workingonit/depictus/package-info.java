9d44a3eef8d0a1f9dc3992b095c281a11e778743
vladimir <vbossica@gmail.com>
2010-05-02 00:46:22 +0200

    change license to LGPL

--- depictus/src/java/org/workingonit/depictus/package-info.java
+++ depictus/src/java/org/workingonit/depictus/package-info.java
@@ -1,23 +1,20 @@
 /*
- * Copyright 2008-2009 Vladimir Ritz Bossicard
+ * Copyright 2008-2010 Vladimir Ritz Bossicard
  *
- * This file is part of WorkingOnIt.
+ * This file is part of WorkingOnIt Depictus.
  *
- * WorkingOnIt is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * WorkingOnIt Depictus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Version     : $Revision: 155 $
- * Last edit   : $Date: 2009-05-18 22:53:11 +0200 (Mon, 18 May 2009) $
- * Last editor : $Author: vbossica $
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
